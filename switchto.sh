41088ddc38da9e8969a8c612ba733be24bfa1272
dylanh <dylan.humphreys@comodo.com>
2018-12-05 09:35:47 +0000

    hack to get evolution working. Not sure if the problem is flatpak, or evolution itself

--- switchto.sh
+++ switchto.sh
@@ -6,12 +6,17 @@
 
 
 # FOR THE FUNCTION:
-# $1 is the list of PIDS we need to make sure we're grepping the right window IDs
+# $1 is the list of PIDS we need to make sure we're grepping the right window IDs. If this is set to "skip" the PIDS wont be cross referenced. This MIGHT result in incorrect switching,
+#	if for example you are editing a document called $2, then switchto might switch to that instead of the application you intended.
+# 	This here purely for evolution. I suspect the real cause is running it via flatpak
 # $2 is the string to grep for in the titlebar of the windows.
 # $3 is the desktop id on which to search
 function switchfocus {
-	# ${3}|
-	WID=$(${WMCTRL} -lp | ${GREP} -E "[0-9]x[0-9a-z]{8}\s(-1|\s${3})" | ${GREP} -iE "${2}" | ${GREP} -E " ${1} " | ${AWK} '{print $1}')
+	if [[ "${1}" != "skip" ]]; then
+		WID=$(${WMCTRL} -lp | ${GREP} -E "[0-9]x[0-9a-z]{8}\s(-1|\s${3})" | ${GREP} -iE "${2}" | ${GREP} -E " ${1} " | ${AWK} '{print $1}')
+	 else
+		WID=$(${WMCTRL} -l | ${GREP} -E "[0-9]x[0-9a-z]{8}\s(-1|\s${3})" | ${GREP} -iE "${2}" | ${AWK} '{print $1}')
+ 	fi
 	log "WID is ${WID}"
 	if [[ ! -z $WID ]]; then 
 	  log "Got the WID of a suitable candidate, switching to that"
@@ -40,11 +45,15 @@ log_err() { [[ -t 2 ]] && >&2 echo "$@" || logger -t $(basename $0) -p user.err 
 #TODO make the logger commands only log if a certain flag is set.
 log "switchto launched with ${1} and ${3}"
 
-PIDS=$(${PIDOF} ${3} | ${SED} 's/\s/|/g')
+if [ "${2}" == "skip" ]; then
+	PIDS="skip"
+ else
+	PIDS=$(${PIDOF} ${3} | ${SED} 's/\s/|/g')
+fi
 
 if [[ ! -z ${PIDS} ]]; then
 	log "PIDS is $PIDS" 
-	switchfocus ${PIDS} ${1} ${DID};
+	switchfocus ${PIDS} ${1} ${DID}
 	if [ "$?" = "1" ]; then
 		# it all went well
 		log "Switched to the window for ${1} on ${DID}"
@@ -52,24 +61,7 @@ if [[ ! -z ${PIDS} ]]; then
 	fi
 fi
 
-# hack for multiple sublime instances. By Default sublime will only load one instance of the editor. As such, if you are on a desktop without an active sublime instance, it will simply put focus on the instance on the other desktop (assuming it exists) cheerfully leaving you on the sublime-less desktop. This hack loads a new instance on the current desktop
-# remove this while if block if you dont use sublime
-if [[ -z $WID ]]; then
-	log "Checking if its sublime or not"
-	BINNAME=$(basename ${3})
-	log "Binname is ${BINNAME}"
-	if [ "${BINNAME}" = "subl3" ]; then
-	# it is sublime, so we'll launch a new instance here with the new instance flag, and then make a call switch to it
-		log "Launching sublime with -n"
-		/usr/bin/subl3 -n &
-		# now switch to it. We need to wait for it to load though
-		# TODO this is crude.
-		sleep 0.25 
-		switchfocus ${PIDS} ${1} ${DID}
-		exit 0
-	fi
-fi
 # its something else that can launch multiple instances without pointless command line switches
 # the application is either not running, or not on this desktop, so launch another
-log "The application is running but on a different desktop, so run it on this one"
+log "The application is either not running or running on a different desktop, so run it on this one"
 ${3} &
